764b20d5da1a17c4d2783bef1a2eb08079f0bd25
lsw0604 <57swji@gmail.com>
2024-09-17 21:37:49 +0900

    [수정] search.schema

--- schemas/search.schema.ts
+++ schemas/search.schema.ts
@@ -3,10 +3,7 @@ import { z } from 'zod';
 export type SearchType = z.infer<typeof searchSchema>;
 
 export const searchSchema = z.object({
-  search: z.string().min(1, {
+  query: z.string().min(1, {
     message: '검색어를 입력해주세요.',
   }),
-  sort: z.enum(['accuracy', 'latest']),
-  size: z.number().min(10).max(50),
-  target: z.enum(['title', 'isbn', 'publisher', 'person']),
 });
