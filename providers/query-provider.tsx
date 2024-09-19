68d87e0600ae2fe109acf23b9c626d31f5a0e10e
lsw0604 <57swji@gmail.com>
2024-09-19 21:23:40 +0900

    [수정] Provider

---
+++ providers/query-provider.tsx
@@ -0,0 +1,12 @@
+import { QueryClientProvider } from '@tanstack/react-query';
+import { queryClient } from 'queries';
+
+interface QueryProviderProps {
+  children: JSX.Element;
+}
+
+export default function QueryProvider({ children }: QueryProviderProps) {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+}
--- providers/redux-provider.tsx
+++ providers/redux-provider.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react';
 import { Provider } from 'react-redux';
-import { store } from '.';
+import { store } from '@/store';
 
 interface ReduxProviderProps {
   children: ReactNode;
