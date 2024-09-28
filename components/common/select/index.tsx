d357c3462dc813901abf03185f571ac592c4fdc5
lsw0604 <57swji@gmail.com>
2024-09-28 21:06:38 +0900

    [수정] Select 컴포넌트 ErrorBoundary추가

--- components/common/select/index.tsx
+++ components/common/select/index.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import { ErrorBoundary } from 'react-error-boundary';
 import React from 'react';
 import { useOnClickOutside } from 'usehooks-ts';
 import { CheckIcon, ChevronDownIcon } from 'lucide-react';
-import { cn } from '@/utils/cn';
+import { cn } from '@/utils/class-name';
+import Alert from '../alert';
 
 type SelectContextType = {
   isOpen: boolean;
@@ -36,6 +38,7 @@ type SelectComponentProps = React.FC<SelectProps> & {
   Trigger: typeof SelectTrigger;
   Content: typeof SelectContent;
   Option: typeof SelectOption;
+  ErrorBoundary: typeof SelectErrorBoundary;
 };
 
 const Select: SelectComponentProps = ({
@@ -60,7 +63,10 @@ const Select: SelectComponentProps = ({
 
 const SelectTrigger: SelectChildComponentProps = ({ children, className }) => {
   const context = React.useContext(SelectContext);
-  if (!context) throw new Error('SelectTrigger must be used within a Select');
+  if (!context)
+    throw new Error(
+      'SelectTrigger는 반드시 Select 컴포넌트 내부에서 사용되어야 합니다.'
+    );
 
   const { isOpen, setIsOpen, triggerRef } = context;
 
@@ -77,12 +83,12 @@ const SelectTrigger: SelectChildComponentProps = ({ children, className }) => {
       ref={triggerRef}
       onClick={onClickTrigger}
       className={cn(
-        'flex h-10 w-full px-4 py-2 text-sm items-center justify-between rounded-md ', // base styles
-        'border border-gray-300 border-input bg-background', // border styles
-        'ring-offset-background placeholder:text-muted-foreground', // ring styles
+        'flex h-10 w-full px-4 py-2 text-sm items-center justify-between rounded-md', // 기본 스타일
+        'border border-gray-300 border-input bg-background', // 테두리 스타일
+        'ring-offset-background placeholder:text-muted-foreground', // 테두리 스타일
         'focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2', // focus styles
-        'disabled:cursor-not-allowed disabled:opacity-50 cursor-pointer', // disabled styles
-        isOpen && 'ring-2 ring-ring ring-offset-2', // open styles
+        'disabled:cursor-not-allowed disabled:opacity-50 cursor-pointer', // 비활성화 스타일
+        isOpen && 'ring-2 ring-ring ring-offset-2', // 열림 스타일
         className
       )}
     >
@@ -99,7 +105,10 @@ const SelectTrigger: SelectChildComponentProps = ({ children, className }) => {
 
 const SelectContent: SelectChildComponentProps = ({ children, className }) => {
   const context = React.useContext(SelectContext);
-  if (!context) throw new Error('SelectContent must be used within a Select');
+  if (!context)
+    throw new Error(
+      'SelectContent는 반드시 Select 컴포넌트 내부에서 사용되어야 합니다.'
+    );
   const { isOpen, triggerRef, setIsOpen } = context;
   const contentRef = React.useRef<HTMLDivElement | null>(null);
   const onClickContent = React.useCallback(
@@ -124,8 +133,8 @@ const SelectContent: SelectChildComponentProps = ({ children, className }) => {
           aria-modal="true"
           ref={contentRef}
           className={cn(
-            'absolute z-50 min-w-[8rem] w-full overflow-hidden rounded-md mt-2', // base styles
-            'border bg-popover text-popover-foreground shadow-md animate-in fade-in-80', // content styles
+            'absolute z-50 min-w-[8rem] w-full overflow-hidden rounded-md mt-2', // 기본 스타일
+            'border bg-popover text-popover-foreground shadow-md animate-in fade-in-80', // 컨텐츠 스타일
             className
           )}
         >
@@ -138,7 +147,10 @@ const SelectContent: SelectChildComponentProps = ({ children, className }) => {
 
 const SelectOption: SelectOptionProps = ({ children, className, value }) => {
   const context = React.useContext(SelectContext);
-  if (!context) throw new Error('SelectOption must be used within a Select');
+  if (!context)
+    throw new Error(
+      'SelectOption는 반드시 Select 컴포넌트 내부에서 사용되어야 합니다.'
+    );
 
   const { onChange, value: selectedValue, setIsOpen } = context;
 
@@ -151,10 +163,9 @@ const SelectOption: SelectOptionProps = ({ children, className, value }) => {
     <li
       onClick={onClickOption}
       className={cn(
-        'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm', // base styles
-        'outline-none transition-colors focus:bg-accent focus:text-accent-foreground', // focus styles
-        'data-[disabled]:pointer-events-none data-[disabled]:opacity-50', // disabled styles
-        selectedValue === value && 'bg-accent text-accent-foreground',
+        'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 text-sm', // 기본 스타일
+        'outline-none transition-colors focus:bg-accent focus:text-accent-foreground', // focus 스타일
+        selectedValue === value && 'bg-accent text-accent-foreground', // 선택된 옵션 스타일
         selectedValue !== value && 'pl-8',
         className
       )}
@@ -165,8 +176,30 @@ const SelectOption: SelectOptionProps = ({ children, className, value }) => {
   );
 };
 
+const SelectErrorBoundary = ({ children }: { children: React.ReactNode }) => {
+  const handleError = (error: Error, info: React.ErrorInfo) => {
+    console.error(
+      `SELECT Component [ERROR] ${error.message} ${
+        info.componentStack?.toString() ?? ''
+      }`
+    );
+  };
+
+  return (
+    <ErrorBoundary
+      FallbackComponent={({ error }) => (
+        <Alert status="ERROR" message={error.message} />
+      )}
+      onError={handleError}
+    >
+      {children}
+    </ErrorBoundary>
+  );
+};
+
 Select.Trigger = SelectTrigger;
 Select.Content = SelectContent;
 Select.Option = SelectOption;
+Select.ErrorBoundary = SelectErrorBoundary;
 
 export default Select;
