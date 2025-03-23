fcc72255e0455932ecaf8b312d047556b0291959
lsw0604 <57swji@gmail.com>
2025-03-23 23:56:56 +0900

    [추가] PageContaier

---
+++ components/common/page-container.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from 'react';
+import { cn } from '@/utils/class-name';
+
+interface PageContainerProps {
+  children: ReactNode;
+  className?: string;
+  variant?: 'default' | 'vertical' | 'horizontal';
+}
+
+const PageContainer = ({
+  children,
+  className,
+  variant = 'default',
+}: PageContainerProps) => {
+  return (
+    <div
+      className={cn(
+        'w-full flex-grow',
+        variant === 'vertical' && 'flex flex-col',
+        variant === 'horizontal' && 'flex flex-row',
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default PageContainer;
