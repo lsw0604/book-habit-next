eb0a29d286a3599f6bb6bf10def24d5f64e28832
lsw0604 <57swji@gmail.com>
2025-06-22 22:43:58 +0900

    ♻️refactor(entities/book): store 코드 수정

---
+++ entities/book/store/book.slice.ts
@@ -0,0 +1,30 @@
+import type { RootState } from '@/shared/redux/store';
+import type { Book } from '../model';
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+
+type BookState = {
+  selectedBook: Book | null;
+};
+
+const initialState: BookState = {
+  selectedBook: null,
+};
+
+const bookSlice = createSlice({
+  name: 'book',
+  initialState,
+  reducers: {
+    selectBook(state, action: PayloadAction<Book>) {
+      state.selectedBook = action.payload;
+    },
+    clearSelectedBook(state) {
+      state.selectedBook = null;
+    },
+  },
+});
+
+export const bookSelector = (state: RootState) => state.book;
+
+export const { selectBook, clearSelectedBook } = bookSlice.actions;
+
+export default bookSlice.reducer;
