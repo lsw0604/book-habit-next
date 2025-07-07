69db601bd00c86582a4b05e1b6bd56972119d204
lsw0604 <57swji@gmail.com>
2025-07-07 16:27:20 +0900

    ♻️refactor(single-datepikcer): 코드 수정

--- shared/ui/date-picker/index.tsx
+++ shared/ui/date-picker/index.tsx
@@ -1,2 +1,1 @@
-export { default as SingleDatePicker } from './single-date-picker';
 export { default as RangeDatePicker } from './range-date-picker';
---
+++ shared/ui/single-datepicker.tsx
@@ -0,0 +1,50 @@
+import type { SelectSingleEventHandler } from 'react-day-picker';
+import { CalendarIcon } from 'lucide-react';
+import { ko } from 'date-fns/locale';
+import { format } from 'date-fns';
+
+import { Popover } from './popover';
+import { Calendar } from './calendar';
+import { cn } from '../utils/class-name';
+
+interface SingleDatepickerProps {
+  selected?: Date;
+  onSelect: SelectSingleEventHandler;
+  className?: string;
+}
+
+const SingleDatepicker = ({
+  selected,
+  onSelect,
+  className,
+}: SingleDatepickerProps) => {
+  return (
+    <Popover
+      className={cn(
+        'w-full h-10 border-slate-600 border-2 rounded-lg bg-transparent relative',
+        className
+      )}
+    >
+      <Popover.Trigger className="w-full h-full flex flex-1 items-center justify-center">
+        <div className="h-10 w-10 flex items-center justify-center">
+          <CalendarIcon size={16} className="stroke-slate-600" />
+        </div>
+        <div className="w-full text-sm py-2 pl-1 pr-11 text-center text-slate-600 font-bold">
+          {selected
+            ? format(selected, 'yyyy년 MM월 dd일')
+            : '날짜를 선택해주세요.'}
+        </div>
+      </Popover.Trigger>
+      <Popover.Content className="p-0 z-9999">
+        <Calendar
+          locale={ko}
+          mode="single"
+          selected={selected}
+          onSelect={onSelect}
+        />
+      </Popover.Content>
+    </Popover>
+  );
+};
+
+export { SingleDatepicker };
