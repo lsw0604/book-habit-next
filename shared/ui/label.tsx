f39285e6cf3fb3ebd7fd2db9ab166c83cff3221b
lsw0604 <57swji@gmail.com>
2025-08-18 23:58:07 +0900

    ♻️refactor(label): CSS 수정

--- shared/ui/label.tsx
+++ shared/ui/label.tsx
@@ -1,25 +1,48 @@
 'use client';
 
-import * as React from 'react';
 import * as LabelPrimitive from '@radix-ui/react-label';
 import { cva, type VariantProps } from 'class-variance-authority';
+import * as React from 'react';
+
 import { cn } from '@/shared/utils/class-name';
 
-const labelVariants = cva(
-  'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
-);
+const labelVariants = cva('font-medium leading-none transition-colors', {
+  variants: {
+    size: {
+      sm: 'text-xs',
+      md: 'text-sm',
+      lg: 'text-base',
+    },
+  },
+  defaultVariants: {
+    size: 'md',
+  },
+});
+
+type LabelProps = React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
+  VariantProps<typeof labelVariants>;
+
+const peerDisabledCases =
+  'peer-disabled:opacity-70 peer-disabled:cursor-not-allowed';
+const groupDisabledCases =
+  'group-has-[:disabled]:opacity-70 group-has-[:disabled]:cursor-not-allowed';
 
 const Label = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
+  LabelProps
+>(({ className, size, ...props }, ref) => (
   <LabelPrimitive.Root
     ref={ref}
-    className={cn(labelVariants(), className)}
+    className={cn(
+      labelVariants({ size }),
+      peerDisabledCases,
+      groupDisabledCases,
+      className
+    )}
     {...props}
   />
 ));
-Label.displayName = LabelPrimitive.Root.displayName;
+
+Label.displayName = 'Label';
 
 export { Label };
