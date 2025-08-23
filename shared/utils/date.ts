a132819338cc9e97984b0d3f8eb5984c2dd90825
lsw0604 <57swji@gmail.com>
2025-08-23 12:43:14 +0900

    ✨feat(utils): NormalizedDate 유틸 추가

--- shared/utils/date.ts
+++ shared/utils/date.ts
@@ -1,3 +1,9 @@
+import { parseISO } from 'date-fns';
 
+export const normalizedDate = (date: string | Date): Date => {
+  if (typeof date === 'string') {
+    return parseISO(date);
+  }
 
+  return date;
 };
