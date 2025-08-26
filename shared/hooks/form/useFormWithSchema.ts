d7e2c9fd7b159dfe4e5eb399a330edb80ebdf602
lsw0604 <57swji@gmail.com>
2025-08-26 16:38:27 +0900

    ✨feat(useFormWithSchema): options 추가
    - 기존 useBookSearchFormHandler를 사용해서 URL창과 화면의 값을 관리해옴
    - 복잡하고 유지 보수에 어려움을 겪어 values라는 옵션을 사용해 ract-hook-form의 기능을 사용함

--- shared/hooks/form/useFormWithSchema.ts
+++ shared/hooks/form/useFormWithSchema.ts
@@ -4,13 +4,15 @@ import {
   useForm,
   UseFormReturn,
   DefaultValues,
+  UseFormProps,
+  FieldValues,
 } from 'react-hook-form';
 import { ZodSchema, infer as InferZodType } from 'zod';
 
-interface UseFormWithSchemaOptions<T> {
+interface UseFormWithSchemaOptions<T extends FieldValues>
+  extends Pick<UseFormProps<T>, 'values' | 'mode'> {
   defaultValues: DefaultValues<T>;
-  resetOnSubmit?: boolean; // 제출 후 자동 리셋 옵션
-  mode?: 'onChange' | 'onBlur' | 'onSubmit' | 'onTouched' | 'all';
+  resetOnSubmit?: boolean;
 }
 
 export function useFormWithSchema<T extends ZodSchema>(
@@ -20,18 +22,19 @@ export function useFormWithSchema<T extends ZodSchema>(
   resetForm: () => void;
 } {
   const {
-    mode = 'onSubmit', // 기본값으로 'onSubmit'을 사용하여 입력 중에는 유효성 검사가 실행되지 않도록 함
+    mode = 'onSubmit',
     defaultValues,
     resetOnSubmit = false,
+    values,
   } = options;
 
   const methods = useForm<InferZodType<T>>({
     resolver: zodResolver(schema),
     defaultValues,
     mode,
+    values,
   });
 
-  // 확장된 handleSubmit 함수로 감싸서 제출 후 상태 리셋 처리
   const originalHandleSubmit = methods.handleSubmit;
 
   const handleSubmitWithReset = (
@@ -40,13 +43,11 @@ export function useFormWithSchema<T extends ZodSchema>(
   ) =>
     originalHandleSubmit(data => {
       onValid(data);
-      // resetOnSubmit이 true이면 제출 후 폼 리셋
       if (resetOnSubmit) {
         methods.reset(defaultValues);
       }
     }, onInvalid);
 
-  // 명시적 리셋 함수 추가
   const resetForm = () => methods.reset(defaultValues);
 
   return {
