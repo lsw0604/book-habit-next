13bca0093686ab8dae5f8d311459c585d75c7e8d
lsw0604 <57swji@gmail.com>
2025-08-26 19:36:31 +0900

    ✨feat(book): createMarkUp 함수 추가

--- entities/book/ui/book-card-content.tsx
+++ entities/book/ui/book-card-content.tsx
@@ -1,6 +1,7 @@
 import { HTMLAttributes } from 'react';
 
 import { cn } from '@/shared/utils/class-name';
+import { createMarkUp } from '@/shared/utils/create-mark-up';
 
 import { Book } from '../model';
 
@@ -14,14 +15,23 @@ export function BookCardContent({
   className,
   ...props
 }: BookCardContentProps) {
+  if (book.contents === '') {
+    return (
+      <p
+        className={cn('text-sm font-normal text-gray-800', className)}
+        {...props}
+      >
+        해당 책의 정보가 등록되지 않았습니다.
+      </p>
+    );
+  }
+
   return (
     <p
-      className={cn('text-sm font-normal text-gray-800', className)}
+      className={cn('text-gray-800 text-sm font-normal', className)}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={createMarkUp(book.contents)}
       {...props}
-    >
-      {book.contents === ''
-        ? '해당 책의 정보가 등록되지 않았습니다.'
-        : book.contents}
-    </p>
+    />
   );
 }
