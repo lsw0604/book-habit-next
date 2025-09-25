d2390e8568401bcf00aacfb448d08e5b89e8e071
lsw0604 <57swji@gmail.com>
2025-09-25 15:31:51 +0900

    ✨feat(my-book-reivew): useMyBookReview 코드 추가:

---
+++ entities/my-book-review/hooks/useMyBookReview.ts
@@ -0,0 +1,22 @@
+import { useQuery } from '@tanstack/react-query';
+import type { AxiosError } from 'axios';
+
+import type { ErrorResponseDTO } from '@/shared/api/types/error';
+import { queryKeys } from '@/shared/query';
+
+import { type MyBookReviewDTO, myBookReviewService } from '../api';
+import { toMyBookReviewViewModel } from '../model/my-book-review.mapper';
+import type { MyBookReview } from '../model/my-book-review.model';
+
+export const useMyBookReview = (myBookId: number) => {
+  const { getMyBookReview } = myBookReviewService;
+
+  return useQuery<MyBookReviewDTO, AxiosError<ErrorResponseDTO>, MyBookReview>({
+    queryKey: queryKeys.myBookReview.detail(myBookId).queryKey,
+    queryFn: () => getMyBookReview(myBookId),
+    select: response => toMyBookReviewViewModel(response),
+    gcTime: 30 * 60 * 1000,
+    staleTime: 10 * 60 * 1000,
+    retry: false,
+  });
+};
