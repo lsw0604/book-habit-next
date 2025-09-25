6531307a78fad2a509f4038b8fe19a0f601d20ac
lsw0604 <57swji@gmail.com>
2025-09-25 22:57:04 +0900

    🔥chore(shared/assets): 파일 이동 및 제거

---
+++ shared/assets/icons/characters/index.ts
@@ -0,0 +1,5 @@
+export { default as CharacterAnnoyed } from './character-annoyed.svg';
+export { default as CharacterMain } from './character-main.svg';
+export { default as CharacterParty } from './character-party.svg';
+export { default as CharacterSad } from './character-sad.svg';
+export { default as CharacterSub } from './character-sub.svg';
---
+++ shared/assets/icons/index.ts
@@ -0,0 +1,4 @@
+export { default as FemaleIcon } from './icon-female.svg';
+export { default as MaleIcon } from './icon-male.svg';
+export { default as KakaoIcon } from './icon-kakao.svg';
+export * from './characters';
