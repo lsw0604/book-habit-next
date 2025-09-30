8bbc489ea48bd95da02a965d671baf53de197fef
lsw0604 <57swji@gmail.com>
2025-09-30 13:26:13 +0900

    ♻️refactor(button): 코드 수정
    -  size props 수정

--- shared/ui/button.tsx
+++ shared/ui/button.tsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 
 import { cn } from '@/shared/utils/class-name';
 
-import Loader from '../common/loader';
+import { Spinner } from './spinner';
 
 const buttonVariants = cva(
   'inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50',
@@ -26,15 +26,14 @@ const buttonVariants = cva(
         none: '',
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
+        sm: 'h-8 px-2 py-1 rounded-sm',
+        md: 'h-10 px-4 py-2 rounded-md',
+        lg: 'h-12 px-8 py-4 rounded-lg',
       },
     },
     defaultVariants: {
       variant: 'default',
-      size: 'default',
+      size: 'md',
     },
   }
 );
@@ -55,7 +54,7 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
         ref={ref}
         {...props}
       >
-        {isLoading ? <Loader /> : props.children}
+        {isLoading ? <Spinner size={size} /> : props.children}
       </Comp>
     );
   }
