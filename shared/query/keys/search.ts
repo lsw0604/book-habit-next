892f38b7e4602215593ca7964262173e04be4489
lsw0604 <57swji@gmail.com>
2025-10-01 19:30:44 +0900

    ♻️refactor(shared/query): 코드 수정
    - 제네릭 타입 수정

--- shared/query/keys/search.ts
+++ shared/query/keys/search.ts
@@ -1,9 +1,7 @@
 import { createQueryKeys } from '@lukemorales/query-key-factory';
 
-import { SearchPayload } from '@/entities/book';
-
 export const searchQueryKeys = createQueryKeys('search', {
-  book: (params: Omit<SearchPayload, 'page'>) => ({
+  book: <T extends object>(params: Omit<T, 'page'>) => ({
     queryKey: [params],
   }),
 });
