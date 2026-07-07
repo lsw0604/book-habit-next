6e594755e35fd230facd905de5113aa7a9ba5292
이성원 <57swji@gmail.com>
2026-07-07 15:23:19 +0900

    🐛fix(user): userSelector의 상태 타입을 auth로 수정하여 타입 에러 해결

--- entities/user/store/user.selector.ts
+++ entities/user/store/user.selector.ts
@@ -1,4 +1,4 @@
 import { UserState } from './user.slice';
 
-export const userSelector = <T extends { user: UserState }>(state: T) =>
-  state.user;
+export const userSelector = <T extends { auth: UserState }>(state: T) =>
+  state.auth;
